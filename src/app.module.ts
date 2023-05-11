e13e1bffe3a56a8f246e50f89b48139ce053e4ac
Firzenax <louis.20160437@gmail.com>
2023-05-11 11:05:11 +0200

    set project structure

---
+++ src/app.module.ts
@@ -0,0 +1,56 @@
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { secret } from './utils/constant';
+import { join } from 'path/posix';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { VideoController } from './controllers/video.controller';
+import { VideoService } from './services/video.service';
+import { UserService } from './services/user.service';
+import { UserController } from './controllers/user.controller';
+import { Video, VideoSchema } from './models/video.schema';
+import { User, UserSchema } from './models/user.schema';
+import { isAuthenticated } from './app.middleware';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
+    MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './public',
+        filename: (req, file, cb) => {
+          const ext = file.mimetype.split('/')[1];
+          cb(null, `${uuidv4()}-${Date.now()}.${ext}`);
+        },
+      }),
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
+  controllers: [AppController, VideoController, UserController],
+  providers: [AppService, VideoService, UserService],
+})
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(isAuthenticated)
+      .exclude({ path: 'api/v1/video/:id', method: RequestMethod.GET })
+      .forRoutes(VideoController);
+  }
+}
